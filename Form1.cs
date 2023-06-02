901c16a2f54e73736276834440711ef362bbab29
Roman <roman.abril13@hotmail.com>
2023-06-02 19:03:37 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Ejemplo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //Thread.Sleep(5000);
+            // MessageBox.Show("Terminó la suspensión de 5 segundos");
+            gif.Visible = true;
+            await Task.Delay(5000);
+            gif.Visible = false;
+        }
+    }
+}
